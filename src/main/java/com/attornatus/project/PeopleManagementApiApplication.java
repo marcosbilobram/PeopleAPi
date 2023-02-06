f8464e32bc58ddb43c2836de6ec1491582d4b6f5
marcosbilobram <marcosbilobram@hotmail.com>
2023-02-06 14:46:27 -0300

    initial commit

---
+++ src/main/java/com/attornatus/project/PeopleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attornatus/project/PeopleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
