ecec045f835d4f563d9e8765cecc3a5311f76fe2
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-09-26 16:22:11 -0300

    Starting

--- src/main/java/com/attornatus/project/dto/AddressDTO.java
+++ src/main/java/com/attornatus/project/dto/AddressDTO.java
@@ -2,83 +2,22 @@ package com.attornatus.project.dto;
 
 import com.attornatus.project.entities.Address;
 import com.attornatus.project.entities.Person;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class AddressDTO {
 
     private Long id;
-    private String publicPlace; //logradouro
+    private String street; //logradouro
     private Integer number; //número
     private String zipCode; //CEP
     private String city; //cidade
     private Boolean isMain;
-
     private Person person; //classe pessoa
-
-    public AddressDTO() {
-    }
-
-    public AddressDTO(Address address) {
-        id = address.getId();
-        publicPlace = address.getPublicPlace();
-        number = address.getNumber();
-        zipCode = address.getZipCode();
-        city = address.getCity();
-        isMain = address.getMain();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPublicPlace() {
-        return publicPlace;
-    }
-
-    public void setPublicPlace(String publicPlace) {
-        this.publicPlace = publicPlace;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Boolean getMain() {
-        return isMain;
-    }
-
-    public void setMain(Boolean main) {
-        isMain = main;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
 }
--- src/main/java/com/attornatus/project/entities/Address.java
+++ src/main/java/com/attornatus/project/entities/Address.java
@@ -3,12 +3,19 @@ package com.attornatus.project.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-//Classe de Endereço
 @Entity
 @Table(name = "tb_address")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address implements Serializable {
 
     @Id
@@ -16,7 +23,7 @@ public class Address implements Serializable {
     private Long id;
 
     @Column(length = 45, nullable = false)
-    private String publicPlace; //logradouro
+    private String street; //logradouro
 
     @Column(nullable = false)
     private Integer number; //número
@@ -37,74 +44,4 @@ public class Address implements Serializable {
     )
     @JsonIgnore
     private Person person; //classe pessoa
-
-    public Address() {
-    }
-
-    public Address(Long id, String publicPlace, Integer number, String zipCode, String city, Person person, Boolean isMain) {
-        this.id = id;
-        this.publicPlace = publicPlace;
-        this.number = number;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.person = person;
-        this.isMain = isMain;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPublicPlace() {
-        return publicPlace;
-    }
-
-    public void setPublicPlace(String publicPlace) {
-        this.publicPlace = publicPlace;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Boolean getMain() {
-        return isMain;
-    }
-
-    public void setMain(Boolean main) {
-        isMain = main;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
-
 }
