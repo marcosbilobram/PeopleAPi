30d60f288b33bc99469a429a9fa902a9794cdb28
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-10-02 23:37:46 -0300

    Create DocumentationConfig.java

---
+++ src/main/java/com/attornatus/project/config/DocumentationConfig.java
@@ -0,0 +1,26 @@
+package com.attornatus.project.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DocumentationConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("People API")
+                        .version("v1")
+                        .description("API de controle de pessoas")
+                        .contact(new Contact().name("Marcos Bilobram").email("marcosbilobram@hotmail.com"))
+                        .contact(new Contact()).title("Linkedin").description("https://www.linkedin.com/in/marcosbilobram/")
+                );
+    }
+
+}
